e57ed873c6318a87abfb2098cc98586b2aa56a94
Samuel de Sousa Sidrone <samuelsousasidrone@gmail.com>
2024-08-05 16:02:00 -0300

    handelogin and sendMessage add

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,10 @@ const login = document.querySelector(".login")
 const loginForm = login.querySelector(".login__form")
 const loginInput = login.querySelector(".login__input")
 
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+
 const colors = [
     "DeepSkyBlue",
     "Turquoise",
@@ -18,20 +22,40 @@ const colors = [
     "Salmon",
     "Gold"
 ]
+const user = { id: "", name: "", color: "" }
+
+let websocket
 
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length)
     return colors[randomIndex]
 }
 
-const user = { id: "", name: "", color: "" }
+const processMessage = ({ data }) => {
+    console.log(data)
+}
 
-const handleSubmit = (event) => {
+const handleLogin = (event) => {
     event.preventDefault()
+
     user.id = crypto.randomUUID()
     user.name = loginInput.value
     user.color = getRandomColor()
-    console.log(user)
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+
+    websocket.onopen = () => websocket.send(`Usuario: ${user.name} entrou no chat`)
+
 }
 
-loginForm.addEventListener("submit", handleSubmit)
+const sendMessage = (event) => {
+    event.preventDefault()
+    websocket.send(chatInput.value)
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
--- src/server.js
+++ src/server.js
@@ -3,16 +3,16 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-const wss = new WebSocketServer({port: process.env.PORT || 8080})
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
-wss.on("conection", (ws) =>{
+wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
-    ws.on("message", (data) => {
-        wss.clients.forEach((clients) => clients.send(data.toString()))
+    ws.send("Bem vindo ao Chat")
 
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
     })
 
-    console.log("Client Connected")
+    console.log("client connected")
 })
-
